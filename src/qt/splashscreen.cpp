0d06dae3987af00620b6000c1af254ed2021179b
News24coin <62815553+News24coin@users.noreply.github.com>
2020-04-29 16:36:17 +0200

    Add files via upload

---
+++ src/qt/splashscreen.cpp
@@ -0,0 +1,174 @@
+// Copyright (c) 2016-2019 Duality Blockchain Solutions Developers
+// Copyright (c) 2014-2019 The Dash Core Developers
+// Copyright (c) 2009-2019 The Bitcoin Developers
+// Copyright (c) 2009-2019 Satoshi Nakamoto
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#include "splashscreen.h"
+#include "networkstyle.h"
+
+
+#include "clientversion.h"
+#include "guiutil.h"
+#include "init.h"
+#include "ui_interface.h"
+#include "util.h"
+#include "version.h"
+#ifdef ENABLE_WALLET
+#include "wallet.h"
+#endif
+
+#include <QApplication>
+#include <QCloseEvent>
+#include <QDesktopWidget>
+#include <QPainter>
+
+SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) : QWidget(0, f), curAlignment(0)
+{
+    // transparent background
+    setAttribute(Qt::WA_TranslucentBackground);
+    setStyleSheet("background:transparent;");
+
+    // no window decorations
+    setWindowFlags(Qt::FramelessWindowHint);
+
+    QString font = QApplication::font().toString();
+
+    // networkstyle.cpp can't (yet) read themes, so we do it here to get the correct Splash-screen
+    QString splashScreenPath = ":/images/bitcoin";
+    if(GetBoolArg("-regtest", false))
+        splashScreenPath = ":/images/bitcoin_testnet";
+    if(GetBoolArg("-testnet", false))
+        splashScreenPath = ":/images/bitcoin_testnet";
+    if(GetBoolArg("-privatenet", false))
+        splashScreenPath = ":/images/bitcoin_testnet";
+
+    // load the bitmap for writing some text over it
+    pixmap = networkStyle->getSplashImage();
+
+    QPainter pixPaint(&pixmap);
+    pixPaint.setPen(QColor(100, 100, 100));
+
+    pixPaint.end();
+
+    // Resize window and move to center of desktop, disallow resizing
+    QRect r(QPoint(), pixmap.size());
+    resize(r.size());
+    setFixedSize(r.size());
+    move(QApplication::desktop()->screenGeometry().center() - r.center());
+
+    subscribeToCoreSignals();
+    installEventFilter(this);
+}
+
+SplashScreen::~SplashScreen()
+{
+    unsubscribeFromCoreSignals();
+}
+
+bool SplashScreen::eventFilter(QObject* obj, QEvent* ev)
+{
+    if (ev->type() == QEvent::KeyPress) {
+        QKeyEvent* keyEvent = static_cast<QKeyEvent*>(ev);
+        if (keyEvent->text()[0] == 'q' && breakAction != nullptr) {
+            breakAction();
+        }
+    }
+    return QObject::eventFilter(obj, ev);
+}
+
+void SplashScreen::slotFinish(QWidget* mainWin)
+{
+    Q_UNUSED(mainWin);
+
+    /* If the window is minimized, hide() will be ignored. */
+    /* Make sure we de-minimize the splashscreen window before hiding */
+    if (isMinimized())
+        showNormal();
+    hide();
+    deleteLater(); // No more need for this
+}
+
+static void InitMessage(SplashScreen* splash, const std::string& message)
+{
+    QMetaObject::invokeMethod(splash, "showMessage",
+        Qt::QueuedConnection,
+        Q_ARG(QString, QString::fromStdString(message)),
+        Q_ARG(int, Qt::AlignBottom | Qt::AlignHCenter),
+        Q_ARG(QColor, QColor(200, 150, 200)));
+}
+
+static void ShowProgress(SplashScreen* splash, const std::string& title, int nProgress)
+{
+    InitMessage(splash, title + strprintf("%d", nProgress) + "%");
+}
+
+void SplashScreen::setBreakAction(const std::function<void(void)>& action)
+{
+    breakAction = action;
+}
+
+static void SetProgressBreakAction(SplashScreen* splash, const std::function<void(void)>& action)
+{
+    QMetaObject::invokeMethod(splash, "setBreakAction",
+        Qt::QueuedConnection,
+        Q_ARG(std::function<void(void)>, action));
+}
+
+#ifdef ENABLE_WALLET
+void SplashScreen::ConnectWallet(CWallet* wallet)
+{
+    wallet->ShowProgress.connect(boost::bind(ShowProgress, this, _1, _2));
+    connectedWallets.push_back(wallet);
+}
+#endif
+
+void SplashScreen::subscribeToCoreSignals()
+{
+    // Connect signals to client
+    uiInterface.InitMessage.connect(boost::bind(InitMessage, this, _1));
+    uiInterface.ShowProgress.connect(boost::bind(ShowProgress, this, _1, _2));
+
+#ifdef ENABLE_WALLET
+    uiInterface.LoadWallet.connect(boost::bind(&SplashScreen::ConnectWallet, this, _1));
+#endif
+}
+
+void SplashScreen::unsubscribeFromCoreSignals()
+{
+    // Disconnect signals from client
+    uiInterface.InitMessage.disconnect(boost::bind(InitMessage, this, _1));
+    uiInterface.ShowProgress.disconnect(boost::bind(ShowProgress, this, _1, _2));
+#ifdef ENABLE_WALLET
+    Q_FOREACH (CWallet* const& pwallet, connectedWallets) {
+        pwallet->ShowProgress.disconnect(boost::bind(ShowProgress, this, _1, _2));
+    }
+#endif
+}
+
+void SplashScreen::showMessage(const QString& message, int alignment, const QColor& color)
+{
+    curMessage = message;
+    curAlignment = alignment;
+    curColor = color;
+    update();
+}
+
+void SplashScreen::paintEvent(QPaintEvent* event)
+{
+    QPainter painter(this);
+    painter.drawPixmap(0, 0, pixmap);
+    QRect r = rect().adjusted(5, 5, -5, -30);
+    painter.setPen(curColor);
+    painter.drawText(r, curAlignment, curMessage);
+}
+
+void SplashScreen::closeEvent(QCloseEvent* event)
+{
+    StartShutdown(); // allows an "emergency" shutdown during startup
+    event->ignore();
+}
+
